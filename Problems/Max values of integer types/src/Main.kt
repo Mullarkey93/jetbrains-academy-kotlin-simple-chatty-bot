5b927e2142bb77c70736722ff68213c7c39a1ac4
Michael Mullarkey <michael9389@gmail.com>
2020-10-26 16:31:26 +0000

    chore: completed two more stages

---
+++ Problems/Max values of integer types/src/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    println(Byte.MAX_VALUE)  // 2147483647
+    println(Short.MAX_VALUE)  // 2147483647
+    println(Int.MAX_VALUE)  // 2147483647
+    println(Long.MAX_VALUE)  // 2147483647
+
+
+
+}
