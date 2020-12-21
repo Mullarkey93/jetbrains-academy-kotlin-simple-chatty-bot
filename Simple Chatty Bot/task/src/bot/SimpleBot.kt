41f6432dacaf043685b75f2c65e8f94b06d01f81
Michael Mullarkey <michael9389@gmail.com>
2020-12-21 14:04:26 +0000

    chore: finished project

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -11,12 +11,16 @@ fun main() {
     println("Please, remind me your name.")
 
     // reading a name
-
-    println("What a great name you have, {yourName}!")
+    val yourName =  scanner.next()
+    println("What a great name you have, $yourName!")
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
     // reading all remainders
+    val remainder3 = scanner.nextInt()
+    val remainder5 = scanner.nextInt()
+    val remainder7 = scanner.nextInt()
 
-    println("Your age is {yourAge}; that's a good time to start programming!")
+    val yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    println("Your age is $yourAge; that's a good time to start programming!")
 }
